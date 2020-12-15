0f693dbbb6285f7a11fd9e45c884d7d8e7d83c73
MrWoxy <fievezalexandre62@gmail.com>
2020-12-15 10:58:28 +0100

    exo react

--- reactexo/app.js
+++ reactexo/app.js
@@ -3,16 +3,6 @@ class App extends React.Component {
         answer: "",
         comments: [],
     }
-    changeText = (event) => {
-        this.setState({
-            answer: event.target.value
-        })
-    }
-    submit = (event) => {
-        event.preventDefault();
-    }
-
-
 
     render(){
         return(
@@ -21,7 +11,7 @@ class App extends React.Component {
                     <h3>Say something</h3>
                     <input id="name" type="text" value={this.state.answer} onChange={this.changeText} placeholder="Your Name"/>
                     <textarea placeholder="Your Comment" rows="10"></textarea>
-                    <button id="comment">Comment</button>
+                    <input id="comment" type="submit" value="Envoyer">
                 </div>
             </form> 
         )
@@ -30,3 +20,60 @@ class App extends React.Component {
 }
 
 ReactDOM.render(<App/>, document.getElementById('app'));
+
+class App extends React.Component {
+    state = {
+        name: "",
+        message: "",
+        comments: []
+    }
+
+    handleName =(event) => {
+        console.log("name: " + event.target.value);
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleMessage = (event) => {
+        console.log("message: "+ event.target.value);
+        this.setState({
+            message: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        const comment = {
+            name: this.state.name,
+            message: this.state.message
+        }
+        console.log(comment);
+
+        const tmp = this.state.comments.concat(comment);
+        console.log(tmp);
+
+        this.setState({
+            comments: tmp
+        })
+    }
+
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Nom" 
+                    value={this.state.name}
+                    onChange={this.handleName}
+                     />
+                <textarea placeholder="message" 
+                    value={this.state.message}
+                    onChange={this.handleMessage}
+                    ></textarea>
+                <input type="submit" value="Envoyer" />
+            </form>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById("app"));
